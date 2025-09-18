6c460de617e69a52a8ce746e122b1cd9f349abf1
Ángela ríder <aridjim249@g.educaand.es>
2025-09-18 10:17:15 +0200

    Ejercicio 1 con echo

--- miPrimeraPrueba.php
+++ miPrimeraPrueba.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 <html>
+
 <body>
 
-<h1> Mi primera WEB </h1>
- 
-<?php
-echo "My first PHP script!";
+    <h1> Mi primera WEB </h1>
 
-?>
+    <?php // Inicio de código PHP
+
+    // echo sirve para mostrar información en pantalla.
+    echo "My first PHP script!"; // Las cadenas de texto van entre comillas.
+    // todas las sentencias de PHP deben terminar en punto y coma.
+
+    // Fin de código PHP
+    ?>
+
+    <!-- El código HTML puede ir antes o después del código PHP -->
 
 </body>
+
 </html>
